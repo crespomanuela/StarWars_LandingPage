57f49bbf3ff0bf2ab0013567c8b9fc314b098eaf
Manuela Crespo <manuelacrespo@Manuelas-MacBook-Pro.local>
2026-04-26 19:58:10 -0300

    Finalizando seção Saga e animação da Millennium Falcon

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,15 @@ const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
 
 // Tarefa para compilar o SASS e minificar o CSS
-function styles() {
-    return gulp.src('./src/styles/*.scss') // Busca todos os arquivos .scss
-        .pipe(sass({ outputStyle: 'compressed' })) // Compila e minifica
-        .pipe(gulp.dest('./dist/styles')); // Salva na pasta de destino
+function styles() { 
+    return gulp.src('./src/styles/*.scss') 
+        .pipe(sass({
+            outputStyle: 'compressed' 
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css')); 
 }
 
 function images() {
-    // Adicionei o parâmetro nodir para garantir que ele foque nos arquivos
     return gulp.src('./src/assets/images/**/*', { encoding: false })
         .pipe(gulp.dest('./dist/assets/images'));
 }
@@ -30,7 +31,8 @@ function html() {
 
 // Tarefa de observação (Watch) para desenvolvimento
 exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    // O '**' diz ao Gulp para olhar qualquer pasta dentro de styles
+    gulp.watch('./src/styles/**/*.scss', gulp.parallel(styles)); 
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
     gulp.watch('./src/index.html', gulp.parallel(html));
 }
